2cf6e75d34e8a465649ffa52f7573bb58e908312
ZCNXS <153697689+zcnxs-official@users.noreply.github.com>
2025-07-01 14:04:59 -0400

    Update scan_defender.bat

--- scan_defender.bat
+++ scan_defender.bat
@@ -3,16 +3,21 @@ setlocal enabledelayedexpansion
 
 echo Scanning all drives for Windows Defender...
 
+set "drive="
+
 for %%D in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist %%D:\ (
         if exist "%%D:\Program Files\Windows Defender" (
             echo Windows Defender found on drive %%D:\
-            set drive="%%D"
+            set "drive=%%D:"
         ) else if exist "%%D:\ProgramData\Microsoft\Windows Defender" (
             echo Windows Defender found on drive %%D:\
+            set "drive=%%D:"
         )
     )
 )
 
 echo Scan complete.
+echo %drive% > "%temp%\defender_drive.txt"
+
 pause
